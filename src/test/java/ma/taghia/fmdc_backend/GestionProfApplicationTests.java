b037f188173cb7251c88009c0ec279e64d939787
ZinebTAGHIA <zinebtaghia01@gmail.com>
2023-12-26 19:46:55 +0100

    first commit

---
+++ src/test/java/ma/taghia/fmdc_backend/GestionProfApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.taghia.gestionprof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
